7757bbe48cf49888eea41d1599e27d9e332057f2
Margot Herail <margot.herail@hotmail.fr>
2023-11-07 15:56:19 +0100

    ex12 modification

--- ex12.sql
+++ ex12.sql
@@ -1,4 +1,8 @@
 /* Requête 12 : Trouver le nombre d'entités associées à chaque intermédiaire */
 
-SELECT COUNT(*) AS nb_associations
-FROM assoc_inter_entity;
+SELECT
+  intermediary.name AS intermédiaire,
+  COUNT(assoc_inter_entity.entity) AS nb_entités
+FROM intermediary
+JOIN assoc_inter_entity ON intermediary.id = assoc_inter_entity.inter
+GROUP BY intermediary.name;
