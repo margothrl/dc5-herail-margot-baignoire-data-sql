fc98b252139932d999c0ae74fc8e9aa0b6cb75a3
Margot Herail <margot.herail@hotmail.fr>
2023-11-07 14:15:49 +0100

    ex5 fait

--- ex5.sql
+++ ex5.sql
@@ -1,1 +1,5 @@
 /* Requête 5 : Sélectionner les entités créées après une certaine date (par exemple, le 1er janvier 2000). */
+
+SELECT id, name, jurisdiction, jurisdiction_description, incorporation_date, status, service_provider, note, id_address, end_date, url, lifetime, source
+FROM entity
+WHERE incorporation_date >= '2000-01-01';
