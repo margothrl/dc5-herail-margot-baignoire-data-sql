78559595a14aac059220f56e6dfcee6e42ca8a96
Margot Herail <margot.herail@hotmail.fr>
2023-11-07 14:49:14 +0100

    ex7 fait

--- ex7.sql
+++ ex7.sql
@@ -1,2 +1,5 @@
 /* Requête 7 : Lister les entités et leurs pays d'origine. */
 
+SELECT entity.name as nom , entity.id_address, address.countries as pays
+FROM entity
+JOIN address ON entity.id_address = address.id_address;
