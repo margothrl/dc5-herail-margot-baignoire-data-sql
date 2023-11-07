bf20b94d846ed73b5ab8897618b8d4d965e1e26b
Margot Herail <margot.herail@hotmail.fr>
2023-11-07 15:13:17 +0100

    ex8 fait

--- ex8.sql
+++ ex8.sql
@@ -1,1 +1,9 @@
 /* Requête 8 : Trouver tous les officiers et intermédiaires qui ont une association. */
+
+SELECT intermediary.name AS inter_name, officer.name AS offi_name
+FROM assoc_inter_offi
+JOIN intermediary
+ON intermediary.id = assoc_inter_offi.inter
+JOIN officer
+ON officer.id = assoc_inter_offi.officer;
+
