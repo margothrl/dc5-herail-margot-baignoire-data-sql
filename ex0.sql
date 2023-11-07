83544312aa445f660a935755775c11e89a6c5a3f
Margot Herail <margot.herail@hotmail.fr>
2023-11-07 13:54:09 +0100

    ex1 fait

--- ex0.sql
+++ ex0.sql
@@ -1,2 +1,4 @@
+# Requête 0 : Trouvez tous les pays et les codes connus les afficher en tant que “pays” et “libellé”.
+
 SELECT country, code
 FROM country;
---
+++ ex1.sql
@@ -0,0 +1,4 @@
+# Requête 1 : Sélectionner tous les pays
+
+SELECT country
+FROM country;
